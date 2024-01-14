0b245e2a33a460ab3d078e372f44904b0dee37bc
therealron <rohanj9966@gmail.com>
2024-01-14 18:21:03 +0000

    wip

--- gen_report_service.py
+++ gen_report_service.py
@@ -20,12 +20,20 @@ class QueryItem(BaseModel):
 
 app = FastAPI()
 
+# Configure CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 @app.post("/generate_report/")
 async def create_report(query: QueryItem):
     print(query)
     return item
 
-@app.post("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
