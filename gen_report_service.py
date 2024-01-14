ec1c1bba63a279436fb0c68cf613ec8be44b3c22
therealron <rohanj9966@gmail.com>
2024-01-14 17:48:45 +0000

    wip

---
+++ gen_report_service.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+from gpt_researcher import GPTResearcher
+import asyncio
+
+
+async def get_report(query: str, report_type: str) -> str:
+    researcher = GPTResearcher(query, report_type)
+    report = await researcher.run()
+    return report
+
+
+class QueryItem(BaseModel):
+    query: str
+    # description: str | None = None
+    # price: float
+    # tax: float | None = None
+
+
+app = FastAPI()
+
+
+@app.post("/generate_report/")
+async def create_report(query: QueryItem):
+    print(query)
+    return item
--- temp.py
+++ temp.py
@@ -8,7 +8,8 @@ async def get_report(query: str, report_type: str) -> str:
     return report
 
 if __name__ == "__main__":
-    query = "When people say that the fed is increasing interest rates, what does it mean? Create a report about these interest rates?"
+    # query = "Talk about companies that raised more than a billion dollars but still failed. What are possible reasons that they failes? In cases where its possible, what could the founder have done differently?"
+    query = "Name 5 companies that failed even after raising more than a billion dollars. For each of them, explain what went wrong and what they could have done differently."
     report_type = "research_report"
 
     report = asyncio.run(get_report(query, report_type))
